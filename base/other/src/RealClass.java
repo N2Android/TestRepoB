274b11c21621b960d17e7017174e3231866b24bc
Thean Hoogenboezem <theunis.hoogenboezem@standardbank.co.za>
2018-04-09 18:49:33 +0200

    Created another real class instance for testing purposes

---
+++ base/other/src/RealClass.java
@@ -0,0 +1,26 @@
+import com.sun.istack.internal.logging.Logger;
+import com.sun.javafx.util.Logging;
+
+import sun.rmi.runtime.Log;
+import sun.util.logging.resources.logging;
+
+public class RealClass {
+ public static void main(String[] args) {
+    logging.clearCache();
+    Logger.getLogger(componentClass);
+    Logging.getInputLogger().getName();
+    //java.util.logging.Logger.global.wait();
+    
+    String test = "testing string";
+    //TODO - implement class instances
+
+    test("Testing name");
+
+    return;
+ }
+
+ public static boolean test(String name) {
+     //TODO: Do some work here related to testing the name
+     return false;
+ }
+}
